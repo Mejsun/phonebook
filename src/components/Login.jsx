eaca571617fd821af194e850b7056d4d332e08e2
Mejsun <mejsunalghoul@gmail.com>
2022-06-14 15:33:06 +0100

    connected login to the api endpoint

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import {Wrapper, Header, Subheader, Form, Input, PasswordInput, ShowHidePassword, Pass, Button} from '../styles/StyledComps'
 
@@ -6,39 +6,73 @@ import {Wrapper, Header, Subheader, Form, Input, PasswordInput, ShowHidePassword
 function Login() {
 
   const [userData, setUserData] = useState({email: '', password: ''})
-  const [resultData, setResultData] = useState('')
   const [view, setView] = useState(false)
+  const [validationMessage, setValidationMessage] = useState('')
+  
+
+  const addDataIntoCache = (cacheName, url, response) => {
+    // Converting our response into Actual Response form
+    const data = new Response(JSON.stringify(response));
+  
+    if ('caches' in window) {
+      // Opening given cache and putting our data into it
+      caches.open(cacheName).then((cache) => {
+        cache.put(url, data);
+        alert('Data Added into cache!')
+      });
+    }
+  };
 
   async function handleSubmit (e){
     e.preventDefault();
 
-    const res = await axios.post('https://interview.intrinsiccloud.net/auth/login', {
+    await axios.post('https://interview.intrinsiccloud.net/auth/login', {
       username: userData.email,
       password: userData.password
     })
       .then(res => {
-        console.log(res)
+        console.log(res.data)
+        localStorage.setItem('email', userData.email) //save email
+        localStorage.setItem('token', res.data.token) //save email
+        addDataIntoCache('profile data', 'http://localhost:3000/profile', 'sample')
+        //window.location = '/profile'
       })
       .catch(err => {
-        console.log('error')
         console.log(err)
       })
-    return res
+    
   }
 
+  useEffect(() => {
+    const emailRegex = /^[a-zA-Z0-9!#$%&'*+=?^_`{|}~\W]+@[a-zA-Z0-9!#$%&'*+=?^_`{|}~\W]+\.[a-z.]{2,}/gm
+    let validateInfo = '' 
+
+    if(!userData.email){
+      validateInfo ='Please log in below'                 
+    }else{
+      if(userData.email){
+        if(emailRegex.test(userData.email) === false){
+          validateInfo ='Invalid email format'
+        }else{
+          validateInfo ='Valid email format'
+        }
+      }
+    }
+    setValidationMessage(validateInfo)
+  }, [userData.email]) 
 
 
   return (
     <>
     <Wrapper>
       <Header>Welcome!</Header>
-      <Subheader>Some message</Subheader>
+      <Subheader>{validationMessage}</Subheader>
       <Form>
         <label htmlFor='email'>Email</label>
-        <Input type='email' />
+        <Input type='email' onChange={(e)=>{setUserData({...userData, email: e.target.value})}} />
         <label htmlFor='password'>Password</label>
         <PasswordInput>
-          <Pass type={view ? 'text' : 'password'}/>
+          <Pass type={view ? 'text' : 'password'} onChange={(e)=>{setUserData({...userData, password: e.target.value})}}/>
           <ShowHidePassword type='button' onClick={() => setView(!view)}><i className={view ? "fa-solid fa-eye-slash" : "fa-solid fa-eye"}></i></ShowHidePassword>
         </PasswordInput>
       </Form>
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import Menu from './Menu'
 import { Button, Header, Info, MainWrapper, Subheader, UserInfo, Wrapper } from '../styles/StyledComps'
 
 function UserProfile() {
+
+  const user = {
+    "displayName": "string",
+    "emailAddress": "string",
+    "firstName": "string",
+    "joinDate": "2022-06-14T11:24:11.123Z",
+    "lastName": "string",
+    "message": "string",
+    "success": true,
+    "userId": 0 
+  }
+  const getToken = localStorage.getItem('token')
+  const [errorMessage, setErrorMessage] = useState('')
+
+ axios.get('https://interview.intrinsiccloud.net/profile', {
+      displayName: user.displayName,
+      emailAddress: user.emailAddress,
+      firstName: user.firstName,
+      joinDate: user.joinDate,
+      lastName: user.lastName,
+      message: user.message,
+      success: user.success,
+      userId: user.userId,
+    })
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err.response)
+
+      })
+    
   return (
     <MainWrapper>
+    {getToken ? 
+    (
     <Wrapper>
       <img src='#' alt='profilepic'></img>
       <Header>Name Surname</Header>
@@ -16,7 +52,10 @@ function UserProfile() {
       </UserInfo>
       <Button>Change the password</Button>
       <Button>Logout</Button>
-    </Wrapper>
+    </Wrapper>)
+    : 
+    {errorMessage}
+    }
     </MainWrapper>
   )
 }
